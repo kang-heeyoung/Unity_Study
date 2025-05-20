859abbca10002b7d7fedab3fc6023b0c64bb70e2
kang-hyiyoung <68013740+kang-heeyoung@users.noreply.github.com>
2025-05-20 16:21:33 +0900

    [ADD] Character Movement / Ref Type - Value
    ㅁ 캐릭터 이동하는 기능
      - CharacterMovement.cs
    ㅁ 참조타입과 값 타입 차이를 알아보는 코드
      - Study_class.cs
    ㅁ 반환 타입에 대해 알아보는 코드
      - Calculator.cs

---
+++ UnityStudy2D/Assets/02. Scripts/ExampleClass.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class ExampleClass : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
