6de9b281696d8663e07549cb7a92b3052cde2545
kang-hyiyoung <68013740+kang-heeyoung@users.noreply.github.com>
2025-06-23 22:20:24 +0900

    [ADD] 2D 플랫폼 캐릭터 생성

---
+++ unity_1week/Assets/02. Scripts/Platformer/KnightController_Joystick.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class KnightController_Joystick : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ unity_1week/Assets/02. Scripts/Platformer/KnightController_Keyboard.cs
@@ -0,0 +1,96 @@
+using System;
+using UnityEngine;
+
+public class KnightController_Keyboard : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D knightRb;
+
+    private Vector3 inputDir;
+    [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private float jumpPower = 13f;
+
+    private bool isGround;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        knightRb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update() // 일반적인 작업
+    {
+        InputKeyboard();
+    }
+
+    void FixedUpdate() // 물리적인 작업
+    {
+        Move();
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            animator.SetBool("isGround", true);
+            isGround = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            animator.SetBool("isGround", false);
+            isGround = false;
+        }
+    }
+
+    void InputKeyboard()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+
+        inputDir = new Vector3(h, v, 0);
+
+        Jump();
+        SetAnimation();
+
+        
+        if(inputDir.x != 0)
+        {
+            var scaleX = inputDir.x > 0 ? 1 : -1;
+            transform.localScale = new Vector3(scaleX, 1, 1);
+        }
+    }
+
+    void Move()
+    {
+        if (inputDir.x != 0)
+        {
+            knightRb.linearVelocityX = inputDir.x * moveSpeed;
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround)
+        {
+            animator.SetTrigger("Jump");
+            knightRb.AddForceY(jumpPower, ForceMode2D.Impulse);
+        }
+    }
+
+    void SetAnimation()
+    {
+        if (inputDir.x != 0)
+        {
+            var scaleX = inputDir.x > 0 ? 1 : -1;
+            transform.localScale = new Vector3(scaleX, 1, 1);
+
+            animator.SetBool("isRun", true);
+        }
+        else if (inputDir.x == 0)
+            animator.SetBool("isRun", false);
+    }
+}
