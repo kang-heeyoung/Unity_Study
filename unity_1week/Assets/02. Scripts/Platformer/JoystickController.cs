70b0a5525187a58fe506a46cabf079a0fab8b1ca
kang-hyiyoung <68013740+kang-heeyoung@users.noreply.github.com>
2025-06-25 09:34:07 +0900

    [ADD] 조이스틱 구현
    - 26일차 조이스틱으로 움직이는 캐릭터 구현

--- unity_1week/Assets/02. Scripts/Platformer/JoystickController.cs
+++ unity_1week/Assets/02. Scripts/Platformer/JoystickController.cs
@@ -1,9 +1,46 @@
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class JoystickController : MonoBehaviour
+public class JoystickController : MonoBehaviour, IPointerDownHandler, IDragHandler, IPointerUpHandler
 {
-    public void OnLog(string msg)
+    [SerializeField] private KnightController_Joystick knightController;
+
+    [SerializeField] private GameObject backgroundUI;
+    [SerializeField] private GameObject handlerUI;
+
+    private Vector2 startPos, currPos;
+
+
+    void Start()
     {
-        Debug.Log(msg);
+        backgroundUI.SetActive(true);
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        //backgroundUI.SetActive(true);
+        //backgroundUI.transform.position = eventData.position; 
+        startPos = eventData.position;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        // 조이스틱 구현
+        currPos =  eventData.position;
+        Vector2 dragDir = currPos - startPos;
+
+        float maxDist = Mathf.Min(dragDir.magnitude, 75f);
+
+        handlerUI.transform.position = startPos + dragDir.normalized * maxDist;
+
+        // 조이스틱 값 전달
+        knightController.InputJoyStick(dragDir.x, dragDir.y); 
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        knightController.InputJoyStick(0, 0);
+        handlerUI.transform.localPosition = Vector2.zero; // 핸들러가 원점으로 이동
+        //backgroundUI.SetActive(false);
     }
 }
